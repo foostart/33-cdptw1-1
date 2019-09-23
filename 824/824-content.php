836ec8053db34c985f7fa6a6bc0e16a65aacaa69
11TrangNguyen <nguyenthithuytrangdaklak@gmail.com>
2019-09-23 20:49:27 +0700

    824

--- 824/824-content.php
+++ 824/824-content.php
@@ -1,41 +1,58 @@
 <?php
-$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$url_host = $_SERVER['HTTP_HOST'];
+
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
 preg_match_all($pattern_uri, __DIR__, $matches);
+
 $url_path = $url_host . $matches[1][0];
+
 $url_path = str_replace('\\', '/', $url_path);
-
-if (!class_exists('lessc')) {
-    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
-    require_once($dir_block . '/libs/lessc.inc.php');
-}
-
-$less = new lessc;
-$less->compileFile('less/824.less', 'css/styles.css');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>824</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-        <link href="<?php echo $url_path ?>/css/styles.css" rel="stylesheet" type="text/css" />
-        <!--Fontawesome CDN-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-        
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/824.less', 'css/styles.css');
-        ?>
-    </head>
-    <body >
-        <?php include '../824/824.php'; ?>
-    </body>
-</html>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><a href="#"><i class="fa fa-facebook-square"></i></a></span>
+					<span><a href="#"><i class="fa fa-google-plus-square"></i></a></span>
+					<span><a href="#"><i class="fa fa-twitter-square"></i></a></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username" id="username">
+						
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-unlock-alt"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password" id="password">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+						<button id="btnLogin" class="btn btn-login float-right">Login</button>
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="#">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- 824/824.php
+++ 824/824.php
@@ -1,59 +1,40 @@
 <?php
-$url_host = $_SERVER['HTTP_HOST'];
-
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
 preg_match_all($pattern_uri, __DIR__, $matches);
-
 $url_path = $url_host . $matches[1][0];
-
 $url_path = str_replace('\\', '/', $url_path);
-?>
 
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="password">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/824.less', 'css/styles.css');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>824</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $url_path ?>/css/styles.css" rel="stylesheet" type="text/css" />
+    <!--Fontawesome CDN-->
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="<?php echo $url_path ?>/js/jquery.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/824.js"></script>
+    <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php include '../824/824-content.php'; ?>
+
+</body>
+
+</html>
