11142506e9106e58defe05b9e05e4a388e057261
11TrangNguyen <nguyenthithuytrangdaklak@gmail.com>
2019-09-24 11:32:57 +0700

    1173

---
+++ 1173/1173-content.php
@@ -0,0 +1,29 @@
+<?php
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1173">
+    <div class="container">
+
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Form Elements</h3>
+            </div>
+            <div class="title_right">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<div class="clear"></div>
---
+++ 1173/1173.php
@@ -0,0 +1,36 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/1173.less', 'css/1173.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>1173</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/1173.css" rel="stylesheet" type="text/css" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <?php include $dir_block.'/1173-content.php'; ?>
+</body>
+
+</html>
