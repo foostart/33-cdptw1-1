3041db947629b6322bf3fed5ba4137707cf60dec
11TrangNguyen <nguyenthithuytrangdaklak@gmail.com>
2019-09-24 11:22:22 +0700

    1171

--- 1171/1171-content.php
+++ 1171/1171-content.php
@@ -1,48 +1,23 @@
-<!DOCTYPE html>
-
-<html>
-
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/25-5.less', 'css/25-5.css');
-        ?>
-        <link href="css/25-5.css" rel="stylesheet" type="text/css" />
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/skycons.js" type="text/javascript"></script>
-        <script src="js/jquery.vmap.js" type="text/javascript"></script>
-        <script src="js/jquery.vmap.world.js" type="text/javascript"></script>
-        <script src="js/jquery.vmap.sampledata.js" type="text/javascript"></script>
-        <script src="js/custom.min.js" type="text/javascript"></script>
-    </head>
-
-    <body>
-
-        <div class="type-25-5">
+<?php
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+        <div class="type-1171">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-2">
-                    </div>
-                    <div class="col-md-10">
+                    <div class="col-md-10 col-md-offset-2">
                         <div class="content5">
-                            <div class="col-md-4 col-sm-4 col-xs-12">
-                            </div>
-                            <div class="col-md-8 col-sm-8 col-xs-12">
+                            <div class="col-md-8 col-md-offset-4 col-sm-8 col-sm-offset-4 col-xs-12 col-xs-offser-0">
                                 <div class="row">
                                     <div class="col-md-12 col-sm-12 col-xs-12">
                                         <div class="x_panel">
                                             <div class="x_title">
-                                                <h2>Visitors location <small>geo-presentation</small></h2>
+                                                <h2>Visitors location</h2>
                                                 <ul class="nav navbar-right panel_toolbox">
                                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                                     </li>
@@ -89,7 +64,7 @@
                                                         </tbody>
                                                     </table>
                                                 </div>
-                                                <div id="world-map-gdp" class="col-md-8 col-sm-12 col-xs-12" style="height:230px;">
+                                                <div id="world-map-gdp" class="col-md-8 col-sm-12 col-xs-12" style="height:230px; left: 24px;">
                                                 </div>
                                             </div>
                                         </div>
@@ -100,7 +75,7 @@
                                     <div class="col-md-6 col-sm-6 col-xs-12">
                                         <div class="x_panel">
                                             <div class="x_title">
-                                                <h2>To Do List <small>Sample tasks</small></h2>
+                                                <h2>To Do List</h2>
                                                 <ul class="nav navbar-right panel_toolbox">
                                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                                     </li>
@@ -165,7 +140,7 @@
                                     <div class="col-md-6 col-sm-6 col-xs-12">
                                         <div class="x_panel">
                                             <div class="x_title">
-                                                <h2>Daily active users <small>Sessions</small></h2>
+                                                <h2>Daily active users</h2>
                                                 <ul class="nav navbar-right panel_toolbox">
                                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                                     </li>
@@ -194,23 +169,24 @@
                                                 </div>
                                                 <div class="row">
                                                     <div class="col-sm-4">
-                                                        <canvas height="84" width="84" id="partly-cloudy-day"></canvas>
+                                                   
+                                                        <canvas height="84" width="84" style="padding-top: 20px;" id="partly-cloudy-day"></canvas>
                                                     </div>
                                                     <div class="col-sm-8">
                                                         <div class="weather-text">
-                                                            <h2>Texas <br><i>Partly Cloudy Day</i></h2>
+                                                            <h3>Texas Partly Cloudy Day</h3>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="weather-text">
-                                                    <h3 class="degrees">23</h3>
+                                                    <h4 class="degrees">23</h4>
                                                 </div>
                                                 <div class="clearfix"></div>
                                                 <div class="row">
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Mon</h2>
-                                                            <h3 class="degrees">25</h3>
+                                                            <h4 class="degrees">25</h4>
                                                             <canvas id="clear-day" width="32" height="32"></canvas>
                                                             <h5>15 <i>km/h</i></h5>
                                                         </div>
@@ -218,7 +194,7 @@
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Tue</h2>
-                                                            <h3 class="degrees">25</h3>
+                                                            <h4 class="degrees">25</h4>
                                                             <canvas height="32" width="32" id="rain"></canvas>
                                                             <h5>12 <i>km/h</i></h5>
                                                         </div>
@@ -226,7 +202,7 @@
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Wed</h2>
-                                                            <h3 class="degrees">27</h3>
+                                                            <h4 class="degrees">27</h4>
                                                             <canvas height="32" width="32" id="snow"></canvas>
                                                             <h5>14 <i>km/h</i></h5>
                                                         </div>
@@ -234,7 +210,7 @@
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Thu</h2>
-                                                            <h3 class="degrees">28</h3>
+                                                            <h4 class="degrees">28</h4>
                                                             <canvas height="32" width="32" id="sleet"></canvas>
                                                             <h5>15 <i>km/h</i></h5>
                                                         </div>
@@ -242,7 +218,7 @@
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Fri</h2>
-                                                            <h3 class="degrees">28</h3>
+                                                            <h4 class="degrees">28</h4>
                                                             <canvas height="32" width="32" id="wind"></canvas>
                                                             <h5>11 <i>km/h</i></h5>
                                                         </div>
@@ -250,7 +226,7 @@
                                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                                         <div class="daily-weather">
                                                             <h2 class="day">Sat</h2>
-                                                            <h3 class="degrees">26</h3>
+                                                            <h4 class="degrees">26</h4>
                                                             <canvas height="32" width="32" id="cloudy"></canvas>
                                                             <h5>10 <i>km/h</i></h5>
                                                         </div>
@@ -267,6 +243,3 @@
                 </div>
             </div>
         </div>
-    </body>
-
-</html>
---
+++ 1171/1171.php
@@ -0,0 +1,40 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/1171.less', 'css/1171.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>1171</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/1171.css" rel="stylesheet" type="text/css" />
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/skycons.js"></script>
+        <script src="js/jquery.vmap.js"></script>
+        <script src="js/jquery.vmap.world.js"></script>
+        <script src="js/jquery.vmap.sampledata.js"></script>
+        <script src="js/custom.min.js"></script>
+    </head>
+    <body>
+        <?php include $dir_block.'/1171-content.php'; ?>
+    </body>
+</html>
